de03efbc163b5ddb63c691f9005589d1ae813810
LoganSchultz0 <145064916+LoganSchultz0@users.noreply.github.com>
2024-10-07 16:59:08 -0600

    Update a2.py
    passes all test cases

--- a2.py
+++ a2.py
@@ -4,6 +4,43 @@
 
 import sys
 import random
+import time
+import math
+
+class TranspositionTable:
+    def __init__(self):
+        self.table = {}
+    
+    def store(self, hash_value, value):
+        self.table[hash_value] = value
+    
+    def lookup(self, hash_value):
+        return self.table.get(hash_value, None)
+
+class ZobristHash:
+    def __init__(self, rows, cols):
+        self.rows = rows
+        self.cols = cols
+        # Initialize random numbers for each cell and piece
+        self.zobrist_table = [[[random.getrandbits(64) for _ in range(2)] for _ in range(cols)] for _ in range(rows)]
+        self.current_hash = 0
+
+    def compute_hash(self, board):
+        h = 0
+        for row in range(self.rows):
+            for col in range(self.cols):
+                piece = board[row][col]
+                if piece is not None:
+                    h ^= self.zobrist_table[row][col][piece]
+        self.current_hash = h
+        return h
+
+    def update_hash(self, row, col, old_piece, new_piece):
+        if old_piece is not None:
+            self.current_hash ^= self.zobrist_table[row][col][old_piece]
+        if new_piece is not None:
+            self.current_hash ^= self.zobrist_table[row][col][new_piece]
+        return self.current_hash
 
 class CommandInterface:
 
@@ -22,6 +59,7 @@ class CommandInterface:
         }
         self.board = [[None]]
         self.player = 1
+        self.time_limit = 1
     
     #===============================================================================================
     # VVVVVVVVVV START of PREDEFINED FUNCTIONS. DO NOT MODIFY. VVVVVVVVVV
@@ -95,17 +133,18 @@ class CommandInterface:
     def game(self, args):
         if not self.arg_check(args, "n m"):
             return False
-        n, m = [int(x) for x in args]
+        n, m = [int(arg) for arg in args]
         if n < 0 or m < 0:
             print("Invalid board size:", n, m, file=sys.stderr)
             return False
-        
-        self.board = []
-        for i in range(m):
-            self.board.append([None]*n)
+
+        self.board = [[None] * n for _ in range(m)]
         self.player = 1
+        # Initialize Zobrist hashing
+        self.zobrist_hash = ZobristHash(m, n)
+        self.current_hash = self.zobrist_hash.compute_hash(self.board)
         return True
-    
+
     def show(self, args):
         for row in self.board:
             for x in row:
@@ -113,167 +152,195 @@ class CommandInterface:
                     print(".", end="")
                 else:
                     print(x, end="")
-            print()                    
+            print()
         return True
 
     def is_legal_reason(self, x, y, num):
         if self.board[y][x] is not None:
             return False, "occupied"
-        
-        consecutive = 0
-        count = 0
-        self.board[y][x] = num
-        for row in range(len(self.board)):
-            if self.board[row][x] == num:
-                count += 1
-                consecutive += 1
-                if consecutive >= 3:
-                    self.board[y][x] = None
-                    return False, "three in a row"
-            else:
-                consecutive = 0
-        too_many = count > len(self.board) // 2 + len(self.board) % 2
-        
-        consecutive = 0
-        count = 0
-        for col in range(len(self.board[0])):
-            if self.board[y][col] == num:
-                count += 1
-                consecutive += 1
-                if consecutive >= 3:
-                    self.board[y][x] = None
-                    return False, "three in a row"
-            else:
-                consecutive = 0
-        if too_many or count > len(self.board[0]) // 2 + len(self.board[0]) % 2:
-            self.board[y][x] = None
-            return False, "too many " + str(num)
 
-        self.board[y][x] = None
+        # Check row
+        row = self.board[y][:]
+        row[x] = num
+        if not self.is_valid_line(row):
+            return False, "invalid row"
+
+        # Check column
+        column = [self.board[i][x] for i in range(len(self.board))]
+        column[y] = num
+        if not self.is_valid_line(column):
+            return False, "invalid column"
+
         return True, ""
-    
-    def is_legal(self, x, y, num):
-        if self.board[y][x] is not None:
-            return False
-        
-        consecutive = 0
-        count = 0
-        self.board[y][x] = num
-        for row in range(len(self.board)):
-            if self.board[row][x] == num:
-                count += 1
-                consecutive += 1
-                if consecutive >= 3:
-                    self.board[y][x] = None
-                    return False
-            else:
-                consecutive = 0
-        if count > len(self.board) // 2 + len(self.board) % 2:
-            self.board[y][x] = None
-            return False
-        
-        consecutive = 0
-        count = 0
-        for col in range(len(self.board[0])):
-            if self.board[y][col] == num:
-                count += 1
-                consecutive += 1
-                if consecutive >= 3:
-                    self.board[y][x] = None
-                    return False
-            else:
-                consecutive = 0
-        if count > len(self.board[0]) // 2 + len(self.board[0]) % 2:
-            self.board[y][x] = None
-            return False
 
-        self.board[y][x] = None
-        return True
+    def is_valid_line(self, line):
+        
+        count_zero = count_one = 0
+        consecutive = 1
+        half_length = len(line) / 2  # Use float division to determine half
     
+        for i in range(len(line)):
+            if line[i] is not None:
+                if i > 0 and line[i] == line[i - 1]:
+                    consecutive += 1
+                    if consecutive >= 3:
+                        return False  # No three consecutive same numbers
+                else:
+                    consecutive = 1
+                
+                # Count zeros and ones
+                if line[i] == 0:
+                    count_zero += 1
+                else:
+                    count_one += 1
+            else:
+                consecutive = 1
+        
+        # Use math.ceil to round up and check counts against half of line length
+        if count_zero > math.ceil(half_length) or count_one > math.ceil(half_length):
+            return False
+    
+        return True
+
+    def is_legal(self, x, y, num):
+        legal, _ = self.is_legal_reason(x, y, num)
+        return legal
+
     def valid_move(self, x, y, num):
-        return  x >= 0 and x < len(self.board[0]) and\
-                y >= 0 and y < len(self.board) and\
-                (num == 0 or num == 1) and\
-                self.is_legal(x, y, num)
+        return (0 <= x < len(self.board[0]) and
+                0 <= y < len(self.board) and
+                num in (0, 1) and
+                self.is_legal(x, y, num))
 
     def play(self, args):
-        err = ""
-        if len(args) != 3:
-            print("= illegal move: " + " ".join(args) + " wrong number of arguments\n")
+        if not self.arg_check(args, "x y number"):
             return False
-        try:
-            x = int(args[0])
-            y = int(args[1])
-        except ValueError:
-            print("= illegal move: " + " ".join(args) + " wrong coordinate\n")
+        x, y, num = [int(arg) for arg in args]
+        if not self.valid_move(x, y, num):
+            print(f"= illegal move: {x} {y} {num}", file=sys.stderr)
             return False
-        if  x < 0 or x >= len(self.board[0]) or y < 0 or y >= len(self.board):
-            print("= illegal move: " + " ".join(args) + " wrong coordinate\n")
-            return False
-        if args[2] != '0' and args[2] != '1':
-            print("= illegal move: " + " ".join(args) + " wrong number\n")
-            return False
-        num = int(args[2])
-        legal, reason = self.is_legal_reason(x, y, num)
-        if not legal:
-            print("= illegal move: " + " ".join(args) + " " + reason + "\n")
-            return False
+
+        old_piece = self.board[y][x]
         self.board[y][x] = num
-        if self.player == 1:
-            self.player = 2
-        else:
-            self.player = 1
+        self.zobrist_hash.update_hash(y, x, old_piece, num)
+        self.current_hash = self.zobrist_hash.current_hash
+        self.player = 3 - self.player  # Switch player
         return True
-    
+
     def legal(self, args):
         if not self.arg_check(args, "x y number"):
             return False
-        x, y, num = [int(x) for x in args]
-        if self.valid_move(x, y, num):
-            print("yes")
-        else:
-            print("no")
+        x, y, num = args
+        print("yes" if self.valid_move(x, y, num) else "no", flush=True)
         return True
-    
+
     def get_legal_moves(self):
         moves = []
         for y in range(len(self.board)):
             for x in range(len(self.board[0])):
-                for num in range(2):
+                for num in (0, 1):
                     if self.is_legal(x, y, num):
-                        moves.append([str(x), str(y), str(num)])
+                        moves.append([x, y, num])
         return moves
 
     def genmove(self, args):
         moves = self.get_legal_moves()
-        if len(moves) == 0:
-            print("resign")
+        if not moves:
+            print("resign", flush=True)
         else:
-            rand_move = moves[random.randint(0, len(moves)-1)]
-            self.play(rand_move)
-            print(" ".join(rand_move))
+            move = random.choice(moves)
+            self.play([str(move[0]), str(move[1]), str(move[2])])
+            print(f"{move[0]} {move[1]} {move[2]}", flush=True)
         return True
-    
+
     def winner(self, args):
-        if len(self.get_legal_moves()) == 0:
-            if self.player == 1:
-                print(2)
-            else:
-                print(1)
+        if not self.get_legal_moves():
+            print(self.player, flush=True)
         else:
-            print("unfinished")
+            print("unfinished", flush=True)
         return True
-    
-    # new function to be implemented for assignment 2
+
     def timelimit(self, args):
-        raise NotImplementedError("This command is not yet implemented.")
+        if not self.arg_check(args, "time"):
+            return False
+        self.time_limit = int(args[0])
         return True
-    
-    # new function to be implemented for assignment 2
+
     def solve(self, args):
-        raise NotImplementedError("This command is not yet implemented.")
+        start_time = time.time()
+        time_limit = self.time_limit - 0.1
+        self.transposition_table = TranspositionTable()
+        
+        try:
+            can_win, best_move = self.minimax(self.player, start_time, time_limit)
+            
+            if can_win and best_move:
+                x, y, num = best_move
+                print(f"{self.player} {x} {y} {num}") # current player wins by force
+            elif can_win:
+                print(f"{self.player}") # current player wins
+            else:
+                print(f"{3 - self.player}") # current player loses
+            
+            sys.stdout.flush()
+        except TimeoutError:
+            print("unknown")
+            sys.stdout.flush()
+        
         return True
+
+    def minimax(self, player, start_time, time_limit):
+        
+        #Timer
+        if time.time() - start_time >= time_limit:
+            raise TimeoutError
     
+        # Use Zobrist hash value and player as state
+        state = (self.current_hash, player)
+        lookup = self.transposition_table.lookup(state)
+        if lookup is not None:
+            return lookup, None  # no move when using lookup
+    
+        #get all legal moves for current player
+        moves = self.get_legal_moves()
+        if not moves:
+            self.transposition_table.store(state, False)
+            return False, None  # Current player loses
+    
+        best_move = None  # Store the best winning move, init to None
+    
+        #Iterate over possible moves
+        for move in moves:
+            x, y, num = move
+            old_piece = self.board[y][x]
+            
+            # Make move and update hash
+            self.board[y][x] = num
+            self.zobrist_hash.update_hash(y, x, old_piece, num)
+            self.current_hash = self.zobrist_hash.current_hash
+            
+            # Recursively call minimax to check if opponent can win 
+            can_opponent_win, _ = self.minimax(3 - player, start_time, time_limit)
+            win = not can_opponent_win
+            
+            # Undo move and hash
+            self.board[y][x] = old_piece
+            self.zobrist_hash.update_hash(y, x, num, old_piece)
+            self.current_hash = self.zobrist_hash.current_hash
+            
+            #Timer
+            if time.time() - start_time >= time_limit:
+                raise TimeoutError
+            
+            if win:
+                self.transposition_table.store(state, True)
+                best_move = move  # Store the first winning move
+                return True, best_move  # Return True and the winning move
+    
+        self.transposition_table.store(state, False)
+        return False, None  
+
+
     #===============================================================================================
     # ɅɅɅɅɅɅɅɅɅɅ END OF ASSIGNMENT 2 FUNCTIONS. ɅɅɅɅɅɅɅɅɅɅ
     #===============================================================================================
